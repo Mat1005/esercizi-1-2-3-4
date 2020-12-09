e388694fdc9765925e04fb5f620efd15f629b4fe
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2020-12-09 08:03:25 +0100

    Delete esercizio1.py

--- esercizio1.py
+++
@@ -1,6 +0,0 @@
-parola  =  input ( "Inserire una parola." )
-Parola  =  parola [:: - 1 ]
-if  parola  ==  Parola:
-    print ( "La parola inserita è palindroma." )
-else:
-    print ( "La parola inserita non è palindroma." ) 
